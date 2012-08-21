38dc35e410a254e73bdf4087901edad39f1484f0
DTrejo <david.trejo@voxer.com>
2012-08-21 15:36:03 -0700

    tests for get, put, put w/ return_body=true, replace, modify

---
+++ test.js
@@ -0,0 +1,114 @@
+var specify = require("specify"),
+    filters = process.argv.slice(2),
+    RiakClient = require("./"),
+    client;
+
+client = new RiakClient(["127.0.0.1:8098"], "client_id", "pool_name");
+
+// turn this on to see an activity trace
+client.debug_mode = false;
+
+client.on("metrics", function (type, key, val) {
+    console.log("\tmetric: " + type + ", " + key + "=" + val);
+});
+
+client.on("error", function (err) {
+    console.error(new Error(err.message).stack);
+    process.exit(0);
+});
+
+specify("put", function (assert) {
+    var bucket = "bucket_1",
+        key = "key_0",
+        message = "some string full of juicy data",
+        options = {};
+
+    client.put(bucket, key, message, options, function (err, res, obj) {
+        assert.ok(!err);
+        assert.equal(res.statusCode, 204);
+    });
+});
+specify("returnbody", function (assert) {
+    var bucket = "bucket_1",
+        key = "key_0",
+        message = "some string full of juicy data",
+        options = { return_body: true };
+
+    client.put(bucket, key, message, options, function (err, res, obj) {
+        assert.ok(!err);
+        assert.equal(res.statusCode, 200);
+    });
+});
+
+specify("get", function (assert) {
+    var bucket = "bucket_1",
+        key = "key_0",
+        message = "blah blah blah blah riak is cool blah blah",
+        options = {};
+
+    client.put(bucket, key, message, options, function (err, res, obj) {
+        assert.ok(!err);
+        assert.equal(res.statusCode, 204);
+
+        client.get(bucket, key, options, callback);
+    });
+    function callback(err, res, obj) {
+        assert.ok(!err);
+        assert.equal(obj, message);
+        assert.equal(res.statusCode, 200);
+    }
+});
+
+specify("modify", function (assert) {
+    var bucket = "bucket_1",
+        key = "key_1",
+        message = { counter: 0 },
+        options = {};
+
+    client.put(bucket, key, message, options, function (err, res, obj) {
+        assert.ok(!err);
+        assert.equal(res.statusCode, 204);
+
+        // GET + PUT as modify()
+        // This mutator function does a simple increment of obj.counter
+        // with no error checking.
+        client.modify("bucket_1", "key_1", mutator, options, callback);
+
+    });
+    function mutator(old, done) {
+        var newobj = old || {};
+        newobj.counter = newobj.counter || 0;
+        newobj.counter++;
+        done(newobj);
+    }
+    function callback(err, res, obj) {
+        assert.ok(!err);
+        assert.equal(res.statusCode, 204);
+        assert.equal(obj.counter, 1);
+    }
+});
+
+specify("replace", function (assert) {
+    var bucket = "bucket_1",
+        key = "key_2",
+        message = "OLD VAL",
+        new_val = "NEW VAL",
+        options = {};
+    client.put(bucket, key, message, options, function (err, res, obj) {
+        assert.ok(!err);
+        assert.equal(res.statusCode, 204);
+
+        client.replace(bucket, key, new_val, options, callback);
+    });
+    function callback(err, res, obj1) {
+        assert.ok(!err);
+        assert.equal(res.statusCode, 204);
+        assert.equal(obj1, new_val);
+
+        client.get(bucket, key, options, function (err, res, obj2) {
+            assert.equal(obj2, new_val);
+        });
+    }
+});
+
+specify.run(filters);
